b2aa7675ca7a8b62606ee5fba86bf51e3d4fafa6
Emil Zhai <root@derzh.com>
2024-03-18 23:19:12 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""The ups_over_tcp component."""
+
+DOMAIN = "ups_over_tcp"
+
+def setup(hass, config):
+    """Set up the ups_over_tcp component."""
+    # Perform setup tasks here, such as reading the configuration
+    # and setting up any required resources or services.
+
+    # Return boolean to indicate that initialization was successful.
+    return True
