41a83e79128839f0d38c0e5dfba238f8fa4d212f
Emil Zhai <root@derzh.com>
2024-12-16 14:07:51 +0800

    feat: fit HA Core 2025.1 const

--- custom_components/ups_over_network/const.py
+++ custom_components/ups_over_network/const.py
@@ -1,18 +1,18 @@
 from homeassistant.const import (
-    ELECTRIC_POTENTIAL_VOLT,
-    FREQUENCY_HERTZ,
-    TEMP_CELSIUS,
+    UnitOfElectricPotential,
+    UnitOfFrequency,
+    UnitOfTemperature,
     PERCENTAGE
 )
 
 # Define sensor types and units
 SENSOR_DEFINITIONS = {
     "raw": ["Raw", "", "mdi:text-box-outline"],
-    "input_voltage": ["Input Voltage", ELECTRIC_POTENTIAL_VOLT, "mdi:flash"],
-    "fault_voltage": ["Fault Voltage", ELECTRIC_POTENTIAL_VOLT, "mdi:flash-off"],
-    "output_voltage": ["Output Voltage", ELECTRIC_POTENTIAL_VOLT, "mdi:flash"],
+    "input_voltage": ["Input Voltage", UnitOfElectricPotential.VOLT, "mdi:flash"],
+    "fault_voltage": ["Fault Voltage", UnitOfElectricPotential.VOLT, "mdi:flash-off"],
+    "output_voltage": ["Output Voltage", UnitOfElectricPotential.VOLT, "mdi:flash"],
     "load": ["Load", PERCENTAGE, "mdi:gauge"],
-    "frequency": ["Frequency", FREQUENCY_HERTZ, "mdi:current-ac"],
-    "battery_voltage": ["Battery Voltage", ELECTRIC_POTENTIAL_VOLT, "mdi:battery"],
-    "temperature": ["Temperature", TEMP_CELSIUS, "mdi:thermometer"],
+    "frequency": ["Frequency", UnitOfFrequency.HERTZ, "mdi:current-ac"],
+    "battery_voltage": ["Battery Voltage", UnitOfElectricPotential.VOLT, "mdi:battery"],
+    "temperature": ["Temperature", UnitOfTemperature.CELSIUS, "mdi:thermometer"],
 }
